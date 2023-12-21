ad85425f014aafa1b45a29f61b37f6ee4de14aea
juanantoniogit <juanantonio.tecno@gmail.com>
2023-12-21 12:45:19 +0100

    Add files via upload

--- Menus/Juego/Nivel_001/Nivel 001.gd
+++ Menus/Juego/Nivel_001/Nivel 001.gd
@@ -1,6 +1,6 @@
-extends Control
-var Creditos = load("res://MenuCreditos/Creditos.tscn")
-var juego = load("res://Juego/Nivel_001/nivel_001.tscn")
+extends Node2D
+var pausa = load("res://MenuPausa/menu_pausa.tscn")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -9,16 +9,10 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-func _on_button_pressed():
-	
-	get_tree().change_scene_to_packed(Creditos)
-	#get_node(".").queue_free()
-	print ("Ir a Creditos")
-	pass # Replace with function body.
 
 
 func _on_pausa_pressed():
-	get_tree().change_scene_to_packed(juego)
+	get_tree().change_scene_to_packed(pausa)
 	#get_node(".").queue_free()
 	print("Ir a Menu principal")
 	pass # Replace with function body.
